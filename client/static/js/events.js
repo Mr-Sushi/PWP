f1041bafa3ec1025689b8e319bfd336553884237
Mirko Kopsa <mirko.kopsa@gmail.com>
2020-04-26 15:20:03 +0300

    Preparations for posting an event

--- client/static/js/events.js
+++ client/static/js/events.js
@@ -15,3 +15,31 @@ function getResource(href, renderer) {
         error: renderError
     });
 }
+
+function postEvent(event) {
+    event.preventDefault();
+    let data = {};
+    let form = $("form[name='postEventForm']");
+    data.name = $("input[name='eventName']").val();
+    data.time = $("input[name='eventTime']").val();
+    data.description = $("input[name='eventDesc']").val();
+    data.location = $("input[name='eventLocation']").val();
+    data.organization = $("input[name='eventOrg']").val();
+    submitEvent(form.attr("action"), form.attr("method"), data, getSubmittedEvent);
+}
+
+function submitEvent(href, method, item, postProcessor) {
+    $.ajax({
+        url: href,
+        type: method,
+        data: JSON.stringify(item),
+        contentType: PLAINJSON,
+        processData: false,
+        success: postProcessor,
+        error: renderError
+    });
+}
+
+$(document).ready(function () {
+    getResource("http://localhost:5000/eventhub/", renderSensors);
+});
